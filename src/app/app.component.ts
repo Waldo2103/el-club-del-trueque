0df5905cc310f36911002d1054bced918a64242a
Waldo2103 <vega.cristian2103@gmail.com>
2021-04-19 00:57:07 -0300

    splashscreen animado ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { MenuController, Platform } from '@ionic/angular';
 import { AuthService } from './servicios/auth.service';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  showSplash = true;
   public appPages = [
     { title: 'Inicio', url: '/home', icon: 'home', color:"success" },
     { title: 'Perfil', url: '/folder/Perfil', icon: 'person', color:"primary" },
     { title: 'Mis Trueques', url: '/folder/Mis Trueques', icon: 'repeat', color:"danger" },
     { title: 'Mensajes', url: '/folder/Mensajes', icon: 'mail', color:"warning" },
-    { title: 'Mi Album', url: '/folder/Albumes', icon: 'book', color:"secondary" },
-    { title: 'Login', url: '/login', icon: 'person', color:"success" },/*
+    { title: 'Mi Album de Troques', url: '/folder/Albumes', icon: 'book', color:"secondary" }/*,
+    { title: 'Login', url: '/login', icon: 'person', color:"success" },
     { title: 'Login', url: '', func: 'logout()', icon: 'log-out', color:"danger" },
     { title: 'Historial', url: '/folder/Spam', icon: 'time' },*/
   ];
   //public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor(private router: Router, private authService: AuthService,
-      private ctrl: MenuController
-    ) {} 
+  constructor(
+    private router: Router, 
+    private authService: AuthService,
+    private ctrl: MenuController,
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen
+    ) {
+      
+    } 
   ngOnInit(){
+    this.initializeApp()
   }
   logout(){
     this.ctrl.enable(false);
     this.authService.logout();
 }
 
+public initializeApp() {
+  this.platform.ready().then(() => {
+    this.statusBar.styleDefault();
+    setTimeout(() => {
+      // console.log('Desactivo la Splash Screen estatica');
+      this.splashScreen.hide();
+    }, 5000);
 
+    setTimeout(() => {
+      // console.log('Desactivo la Splash Screen animada');
+      this.showSplash = false;
+    }, 5000);
+  });
+}
 
 }
+
 
--- src/app/componentes/prod-alta/prod-alta.component.ts
+++ src/app/componentes/prod-alta/prod-alta.component.ts
@@ -9,13 +9,6 @@ import { album, AlbumesService } from 'src/app/servicios/albumes/albumes.service
 import { producto, ProductosService } from 'src/app/servicios/productos/productos.service';
 import { StorageService } from 'src/app/servicios/storage/storage.service';
 
-export interface Foto {
-  nombre: string[]
-  descripcion: string[]
-  etiquetas: string[]
-}
-
-
 @Component({
   selector: 'app-prod-alta',
   templateUrl: './prod-alta.component.html',
@@ -23,19 +16,14 @@ export interface Foto {
 })
 export class ProdAltaComponent implements OnInit {
 
-  public prod;
-  fotosASub: any = [];
-  public vModo;
-  fotos: any = [];
-  nombre: string[];
-  descripcion: string[];
-  etiquetas: string[];
+  fotosASub: any = [];//se guardan las fotos en el formato de subida a firebase
+  public vModo;//guarda si se abre camara o galeria al crear el component
+  fotos: any = [];//se guardan las fotos en formato de listado
   form: FormGroup;
-  public album: album;
-  public troque: producto;
-  public arrayTroque:Array<producto>;
-  public user;
-  public URLs = []
+  public album: album;//se guarda el album que se creará
+  public troque: producto;//se guarda 1 troque que se creara
+  public user;//guardo los datos del usuario logueado
+  //public mostrarForm: boolean = false; //esto es para que no muestre el formulario si aun no se cargo una foto
   constructor(
     private modal: ModalController,
     private camera: Camera,
@@ -52,16 +40,14 @@ export class ProdAltaComponent implements OnInit {
   }
 
   /**PARA SUBIR ARCHIVO */
-  public mensajeArchivo = 'No hay un archivo seleccionado';
-  public datosFormulario = new FormData();
-  public arrayFotos = [];
   public nombreArchivo = '';
-  public URLPublica = '';
   public porcentaje = 0;
   public finalizado = false;
   
   ngOnInit() {
+    //traigo el usuario logueado con su info
     this.user = JSON.parse(localStorage.getItem("userLogin"));
+    //camara o galeria
     this.modoEntrada();
     this.form = this.fBuilder.group({
       descripcionA: [''],
@@ -73,32 +59,24 @@ export class ProdAltaComponent implements OnInit {
     })
   }
   get getDatos() {
-    //console.log(this.form.get('troques') as FormArray)
     return this.form.get('troques') as FormArray;
   }
 
 
   async altaTroque() {
-
-    this.arrayTroque = [];
-    
     let  f = this.form.value
     let fTroque = f.troques
-     //= this.form.value;
      this.album = {
       nombre: f.nombreA,
       descripcion: f.descripcionA,
       id: this.user.correo+"-"+f.nombreA,
-      imagen: "this.URLs[0]",//CUANDO SE ELIMINE UN PRODUCTO DEL ALBUM HAY QUE ACTUALIZAR QUE LA PRIMERA SIGA EXISTIENDO O ACTUALIZAR
+      imagen: "falta guardar la foto",//(!)CUANDO SE ELIMINE UN PRODUCTO DEL ALBUM HAY QUE ACTUALIZAR QUE LA PRIMERA SIGA EXISTIENDO O ACTUALIZAR
       owner: this.user.correo,
-      //uid: ,
     }
     var cont = 0
     for(let p of fTroque){
       
-      //alert( this.URLs[cont])
       this.troque = {
-      //id: f.
       nombre: p.nombre,
       descripcion: p.descripcion,
       etiquetas: p.etiquetas,
@@ -106,35 +84,29 @@ export class ProdAltaComponent implements OnInit {
       imagen: "",
       album : this.user.correo+"-"+f.nombreA
     }
-    this.arrayTroque.push(this.troque);
     await this.subirArchivo(cont, this.troque)
     cont++
     }
     
-    console.log("form:",fTroque,"Album: ",this.album,"Prod: ",this.arrayTroque)
-    this.albServ.createAlbum(this.album).then(resp=>{
-      this.closeAlbum()
-    })
+    
     
   }
-
+//agrega un formulario de Troque
   addTroque(cant: number) {
+    //this.mostrarForm = true;
     const control = this.form.controls['troques'] as FormArray;
-    //alert(cant+" cant")
     for (let i = 0; i < cant; i++) {
       control.push(this.fBuilder.group({
-        nombre:["", new FormControl( Validators.required)],
-          descripcion:["", new FormControl( Validators.required)],
+        nombre: ["", new FormControl( Validators.required)],
+          descripcion: ["", new FormControl( Validators.required)],
           etiquetas: []
       }))
-      
     }
   }
 
   removeTroque(index: number) {
     const control = <FormArray>this.form.controls['troques'];
     control.removeAt(index)
-    //
     if ( index !== -1 ) {
       this.fotos.splice( index, 1 );
       this.fotosASub.splice( index, 1 );
@@ -142,7 +114,6 @@ export class ProdAltaComponent implements OnInit {
   }
 
   public modoEntrada() {
-    this.prod = this.navParams.get('prod');
     this.vModo = this.navParams.get('modo');
     if (this.vModo === "camera") {
       this.abrirCamara()
@@ -154,7 +125,6 @@ export class ProdAltaComponent implements OnInit {
   }
 
   abrirCamara() {
-    //this.photoService.takePhoto();
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
@@ -174,9 +144,11 @@ export class ProdAltaComponent implements OnInit {
     }, (err) => {
       // Handle error
       alert(err)
+      //si tiro error no entro a form
+      this.modal.dismiss()
     });
   }
-
+//Abre la galeria para seleccionar fotos
   abrirGaleria() {
 
     const options2: ImagePickerOptions = {
@@ -184,74 +156,60 @@ export class ProdAltaComponent implements OnInit {
       allow_video: false,
       outputType: 1
     }
-    this.iPicker.getPictures(options2).then(res => {
-      alert(res.length)
-      this.addTroque(res.length)
+    this.iPicker.getPictures(options2).then((res) => {
+      //agrego un nuevo formulario de troque
+      if (res!=="OK") {//le puse esto porque me paso que devolvia esto a veces al iniar
+        this.addTroque(res.length)
+      
       for (var index = 0; index < res.length; index++) {
         this.fotos.push(('data:image/jpeg;base64,' + res[index]));
         this.fotosASub.push(res[index]);
-        let filename = res[index].substring(res[index].lastIndexOf('/') + 1);
-        let path = res[index].substring(0, res[index].lastIndexOf('/') + 1);
-        this.file.readAsDataURL(path, filename).then(base64string => {
-        })
+        alert(JSON.stringify(this.fotos))
       }
+    }else{
+      this.abrirGaleria()
+    }
     }, (err) => {
       alert("hay algo mal que no anda bien con estas fotos" + (JSON.stringify(err)))
     });
   }
   
-   //Evento que se gatilla cuando el input de tipo archivo cambia
-   public cambioArchivo(event) {
-    if (event.target.files.length > 0) {
-      for (let i = 0; i < event.target.files.length; i++) {
-        this.mensajeArchivo = `Archivo preparado: ${event.target.files[i].name}`;
-        this.nombreArchivo = event.target.files[i].name;
-        //this.datosFormulario.delete('archivo');
-        this.datosFormulario.append('archivo', event.target.files[i], event.target.files[i].name)
-      }
-    } else {
-      this.mensajeArchivo = 'No hay un archivo seleccionado';
-    }
-  }
 
   //Sube el archivo a Cloud Storage
   public subirArchivo(cont, troque) {
     let f;
+    let cant = cont
+    for (let i = 0; i < this.fotosASub.length; i++) {
+      if (i===cont) {
+        f = this.fotosASub[i]
+      }
+    }
+    //genero el nombre del archivo
     var fe = new Date();
     var fec: string = fe.getDate()+"-"+fe.getMonth()+"-"+fe.getUTCFullYear()+"_"+fe.getUTCHours()+fe.getUTCMinutes();
-    //for(let f of this.fotosASub){
-      for (let i = 0; i < this.fotosASub.length; i++) {
-        if (i===cont) {
-          f = this.fotosASub[i]
-        }
+    //firebase no acepta el arroba entonces lo reemplazo
+    this.nombreArchivo =`${this.album.id.replace( '@', '-' ).toLowerCase()}_${cont}_${fec}.jpg`
+    var referencia = this.storageServ.referenciaCloudStorage(`productos/${this.nombreArchivo}`);
+    var tarea = this.storageServ.tareaCloudStorage(this.nombreArchivo, f);
+      //Cambia el porcentaje
+    tarea.percentageChanges().subscribe((porcentaje) => {
+      this.porcentaje = Math.round(porcentaje);
+      if (this.porcentaje == 100) {
+        this.finalizado = true;
       }
-      //alert(JSON.stringify(this.fotosASub))
-      //f = (f as string).split(',', 2)[1];
-      this.nombreArchivo =`${this.album.id.replace( '@', '-' ).toLowerCase()}_${cont}_${fec}+.jpg`
-      //let archivo = this.datosFormulario.get('archivo');
-      var referencia = this.storageServ.referenciaCloudStorage(this.nombreArchivo);
-      var tarea = this.storageServ.tareaCloudStorage(this.nombreArchivo, f);
-       //Cambia el porcentaje
-      tarea.percentageChanges().subscribe((porcentaje) => {
-        this.porcentaje = Math.round(porcentaje);
-        if (this.porcentaje == 100) {
-          this.finalizado = true;
-        }
-      });
-
-      referencia.putString(f, 'base64', { contentType: 'image/jpeg' }).then(async (snapshot) => {
-        alert(troque)
-        troque.imagen = await snapshot.ref.getDownloadURL()
-        if (cont==0) {
-          this.album.imagen = await snapshot.ref.getDownloadURL()
-        }
-        this.prodServ.createProducto(troque);
-        
+    });
+//tomo la referencia de la imagen y creo un troque
+    referencia.putString(f, 'base64', { contentType: 'image/jpeg' }).then(async (snapshot) => {
+      troque.imagen = await snapshot.ref.getDownloadURL()
+      if (cont==0) {
+        this.album.imagen = await snapshot.ref.getDownloadURL()
+        this.albServ.createAlbum(this.album)
       }
-        
-      ) 
-    
-   
+      this.prodServ.createProducto(troque);
+    })
+    if ((cant + 1) === this.fotosASub.length) {
+      this.closeAlbum()
+    }
   }
   closeAlbum(){
     this.modal.dismiss()
